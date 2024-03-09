af70c8862c90cd6de33c769a1e1f2a12a4584ca9
mdshakil106 <150663915+mdshakil106@users.noreply.github.com>
2024-03-09 16:56:11 +0600

    Update Temp clac.c

--- Project C/Temp clac.c
+++ Project C/Temp clac.c
@@ -1,54 +1,12 @@
-/*#include <stdio.h>
+/*
+ * File: temperature_converter.c
+ * Author: Md Shakil
+ * Student ID: 2023200000821
+ * Date: 06 March 2024
+ * Description: This program demonstrates the use of choice, if, else if statements, and functions to calculate temperature in either Fahrenheit or Celsius units. It's a demonstration of conversion between the two units, serving as a temperature converter.
+ */
 
-int main()
-{
-    int choice;
-    float temperature, convertedTemperature;
 
-    // Prompt the user to choose the conversion type
-    printf("Choose the conversion type:\n");
-    printf("1. Celsius to Fahrenheit\n");
-    printf("2. Fahrenheit to Celsius\n");
-    scanf("%d", &choice);
-
-    // Check the user's choice and perform the corresponding conversion
-    switch (choice)
-    {
-    case 1:
-        // Celsius to Fahrenheit conversion
-        printf("Enter temperature in Celsius: ");
-        scanf("%f", &temperature);
-        convertedTemperature = (temperature * 9 / 5) + 32;
-        printf("%.2f Celsius is equal to %.2f Fahrenheit.\n", temperature, convertedTemperature);
-        break;
-    case 2:
-        // Fahrenheit to Celsius conversion
-        printf("Enter temperature in Fahrenheit: ");
-        scanf("%f", &temperature);
-        convertedTemperature = (temperature - 32) * 5 / 9;
-        printf("%.2f Fahrenheit is equal to %.2f Celsius.\n", temperature, convertedTemperature);
-        break;
-    default:
-        printf("Invalid choice. Please enter either 1 or 2.\n");
-    }
-
-    return 0;
-}
-// simplified
-#include <stdio.h>
-
-int main() {
-    float celsius, fahrenheit;
-
-    printf("Enter temperature in Celsius: ");
-    scanf("%f", &celsius);
-
-    fahrenheit = (celsius * 9 / 5) + 32;
-
-    printf("%.2f Celsius is equal to %.2f Fahrenheit.\n", celsius, fahrenheit);
-
-    return 0;
-}*/
 // simplified without changing the functionality
 #include <stdio.h>
 
